6d7d3d7eb91c2d08692436ce45d7d4c0308f118d
RSiopa <siopainacio98@ua.pt>
2021-10-28 18:40:50 +0100

    Just did a bit more, because I didn't want to work on other classes

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 # --------------------------------------------------
-# A python script for complex number operations using named tuples and classes
-# Rafael Inacio Siopa.
-# PSR, November 2021.
+# A python script used for a typing test
+# Rafael Inacio Siopa Nª84905
+# Pedro Miguel Durães de Carvalho Nº84670
+# João Pedro Tira Picos Costa Nunes Nº89201
+# Frederico Ribeiro e Martins Nº92760
+# Grupo 7, PSR, November 2021.
 # --------------------------------------------------
 
 import argparse
 import math
-
+from colorama import Fore, Back, Style
 import readchar
 from time import time
+from random import randint
 
 parser = argparse.ArgumentParser(description='Definition of test mode')
 parser.add_argument('-utm', '--use_time_mode', help='The test runs for MAX_VALUE seconds\n ', action='store_true')
@@ -19,23 +23,33 @@ args = parser.parse_args()
 
 def main():
 
-    if args.use_time_mode:
+    print(Fore.RED + 'PSR ' + Fore.YELLOW + ' Group 7 ' + Fore.BLUE + ' November 2021' + Style.RESET_ALL)
+
+    if args.use_time_mode:          #if the user uses the time mode
         tstop = args.max_value      #max time set
         N = math.inf
-    else:
+        print('Test ends after ' + Fore.GREEN + str(tstop) + Style.RESET_ALL + ' seconds')
+    else:                           #if the user uses the input mode
         tstop = math.inf
-        N = args.max_value      #max inputs set!
+        N = args.max_value          #max inputs set
+        print('Test ends after ' + Fore.GREEN + str(N) + Style.RESET_ALL + ' inputs')
 
     print('Pressione uma tecla para começar.')
-    readchar.readchar()
+    readchar.readchar()             #start of the test
     t1 = time()
     t2 = time()
     count = 0
 
-    while (count < N) and (t2 - t1 < tstop):
-        a = t2 - t1
-        print(a)
-        readchar.readchar()
+    while (count < N) and (t2 - t1 < tstop):    #asks for letters until it hits number of inputs desired or time desired
+        cToType = chr(randint(97, 122))                         #chooses a random lower case letter for the user to type
+        print('Type the letter ' + Fore.GREEN + cToType + Style.RESET_ALL)                   #asks for the random letter
+        cTyped = readchar.readchar()
+        if cTyped == ' ':                                             #if the user uses the spacebar key, the test stops
+            break
+        elif cTyped == cToType:                             #if the user typed the correct letter, letter comes in green
+            print('You typed the letter ' + Fore.BLUE + cTyped + Style.RESET_ALL)
+        else:                                                  ##if the user typed the wrong letter, letter comes in red
+            print('You typed the letter ' + Fore.RED + cTyped + Style.RESET_ALL)
         count = count + 1
         t2 = time()
 
