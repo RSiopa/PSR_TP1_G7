9cf790884b3b257c2e9f08c86fc820ffbbf2adc9
Pedro Carvalho <pmdc@ua.pt>
2021-10-30 15:30:02 +0100

    TP1 - new

--- main.py
+++ main.py
@@ -1,58 +1,76 @@
 #!/usr/bin/python3
-# --------------------------------------------------
-# A python script used for a typing test
-# Rafael Inacio Siopa Nª84905
-# Pedro Miguel Durães de Carvalho Nº84670
-# João Pedro Tira Picos Costa Nunes Nº89201
-# Frederico Ribeiro e Martins Nº92760
-# Grupo 7, PSR, November 2021.
-# --------------------------------------------------
 
 import argparse
 import math
 from colorama import Fore, Back, Style
 import readchar
-from time import time
-from random import randint
+from time import time, ctime
+import random
+import string
+from collections import namedtuple
+
+Input = namedtuple('Input', ['requested', 'received', 'duration'])
 
 parser = argparse.ArgumentParser(description='Definition of test mode')
-parser.add_argument('-utm', '--use_time_mode', help='The test runs for MAX_VALUE seconds\n ', action='store_true')
-parser.add_argument('-mv', '--max_value', type=int, required=True, help='The test runs for MAX_VALUE inputs\n ')
-args = parser.parse_args()
-
+parser.add_argument('-mv', '--max_value', type=int, required=True, help='Max number of seconds for time mode or maximum number of inputs for number of inputs mode.\n ')
+parser.add_argument('-utm', '--use_time_mode', action='store_true', help='Max number of secs for time mode or maximum number of inputs for number of inputs mode.\n ')
+args = vars(parser.parse_args())
+print(args)
 
 def main():
+    seconds = time()
+    print(Fore.RED + 'PARI' + Style.RESET_ALL + ' Typing Test, P2, Grupo 7, ' + ctime(seconds))
+    print('Test running up to ' + str(args['max_value']) + ' seconds.')
 
-    print(Fore.RED + 'PSR ' + Fore.YELLOW + ' Group 7 ' + Fore.BLUE + ' November 2021' + Style.RESET_ALL)
-
-    if args.use_time_mode:          #if the user uses the time mode
-        tstop = args.max_value      #max time set
+    if args['use_time_mode']:
+        tstop = args['max_value']      #max time set
         N = math.inf
-        print('Test ends after ' + Fore.GREEN + str(tstop) + Style.RESET_ALL + ' seconds')
-    else:                           #if the user uses the input mode
+    else:
         tstop = math.inf
-        N = args.max_value          #max inputs set
-        print('Test ends after ' + Fore.GREEN + str(N) + Style.RESET_ALL + ' inputs')
+        N = args['max_value']     #max inputs set!
 
-    print('Pressione uma tecla para começar.')
-    readchar.readchar()             #start of the test
+    print('Press any key to start the test')
+    readchar.readchar()
+    count = 0
     t1 = time()
     t2 = time()
-    count = 0
+    save = []
 
-    while (count < N) and (t2 - t1 < tstop):    #asks for letters until it hits number of inputs desired or time desired
-        cToType = chr(randint(97, 122))                         #chooses a random lower case letter for the user to type
-        print('Type the letter ' + Fore.GREEN + cToType + Style.RESET_ALL)                   #asks for the random letter
-        cTyped = readchar.readchar()
-        if cTyped == ' ':                                             #if the user uses the spacebar key, the test stops
-            break
-        elif cTyped == cToType:                             #if the user typed the correct letter, letter comes in green
-            print('You typed the letter ' + Fore.BLUE + cTyped + Style.RESET_ALL)
-        else:                                                  ##if the user typed the wrong letter, letter comes in red
-            print('You typed the letter ' + Fore.RED + cTyped + Style.RESET_ALL)
+    while (count < N) and (t2 - t1 < tstop):
+        to_type = random.choice(string.ascii_lowercase)
+        print('Type letter ' + to_type)
+        t3 = time()
+        typed = readchar.readchar()
+        t4 = time()
+        if typed == ' ':
+            exit()
+        if typed == to_type:
+            print('You typed letter ' + Fore.GREEN + typed + Style.RESET_ALL)
+        else:
+            print('You typed letter ' + Fore.RED + typed + Style.RESET_ALL)
+
+        save.append(Input(requested = to_type, received = typed, duration = (t4 - t3)))
         count = count + 1
         t2 = time()
 
+    print('Current test duration (' + str(t2-t1) + ') exceeds maximum of ' + str(args['max_value']))
+    print(Fore.BLUE + 'Test Finished!' + Style.RESET_ALL)
+    # Calcular estatísticas
+    print(save)
+    test_duration = t2-t1
+    test_start = ctime(t1)
+    test_end = ctime(t2)
+    number_of_types = len(save)
+    number_of_hits = 0
+    type_average_duration = 0
+    type_hit_average_duration = 0
+    for i in range len(save):
+        if save[i].requested == save[i].received:
+            number_of_hits += 1
+        type_average_duration = save[i].duration + type_average_duration
+
+    type_average_duration = type_average_duration/number_of_types
+    accuracy = number_of_hits/number_of_types
 
 
 if __name__ == '__main__':
